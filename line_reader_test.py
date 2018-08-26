ed42f7e508a2e631ca45df7fe9ac7d277413074c
Michael Cope <mecope21@gmail.com>
2018-08-26 05:23:19 -0400

    New test involving MagicMock, and monkeypatch to simulate an actual call to a file. This is done to simulate pieces of a system that could be too resource intensive to test often, or that might not exist at the time. Test fails, green section is next.

--- line_reader_test.py
+++ line_reader_test.py
@@ -1,7 +1,20 @@
+import pytest
+from unittest.mock import MagicMock
 from line_reader import file_reader
+
 
 def test_can_call_read_from_file():
     fr = file_reader("testname")
+
+
+def test_returns_correct_string(monkeypatch):
+    mock_file = MagicMock()
+    mock_file.readline = MagicMock(return_value="test line")
+    mock_open = MagicMock(return_value=mock_file)
+    monkeypatch.setattr("builtins.open", mock_open)
+    result = file_reader("testname")
+    mock_open.assert_call_once_with("testname", "r")
+    assert result == "testname"
 
 
 
