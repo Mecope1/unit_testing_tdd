bf31224581bc69865a248dc47f0def6c15918d8e
Michael Cope <mecope21@gmail.com>
2018-08-26 04:01:11 -0400

    Results of green phase. Dictionary added to store discount rules. Other functions fleshed out to meet test requirement. Some alterations to code for clarity and style were performed as well.

--- checkout_kata.py
+++ checkout_kata.py
@@ -1,14 +1,14 @@
 
 class checkout_kata:
     class Discount:
-        def __init__(self, nmbr_items, price):
+        def __init__(self, nmbr_items, disc_price):
             self.nmbr_items = nmbr_items
-            self.price = price
+            self.price = disc_price
 
     def __init__(self):
         self.prices = {}
         self.discounts = {}
-        self.total = 0
+        self.items = {}
 
     def add_discount_rule(self, item, nmbr_items, disc_price):
         discount = self.Discount(nmbr_items, disc_price)
@@ -18,8 +18,15 @@ class checkout_kata:
         self.prices[item] = price
 
     def add_item(self, item):
-        self.total += self.prices[item]
+        if item in self.items:
+            self.items[item] += 1
+        else:
+            self.items[item] = 1
 
     def calc_total(self):
-        return self.total
+        total = 0
+        for item, cnt in self.items.items():
+            total += self.prices[item] * cnt
+        return total
+
 
